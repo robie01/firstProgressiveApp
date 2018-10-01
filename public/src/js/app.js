0dcd4858ec9a1c845ecfb22d23db7afbb639db43
robie01 <robie.sun.rs@gmail.com>
2018-10-01 13:07:39 +0200

    fetch basics

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -20,14 +20,26 @@ window.addEventListener('beforeinstallprompt', function (event) {
 
 var promise = new Promise(function (resolve, reject) {
     setTimeout(function () {
-        resolve('This is executed once the timer is done')
+        // resolve('This is executed once the timer is done')
         reject({code: 500, message: 'An error occurred!'});
     }, 3000);
 });
+fetch('https://httpbin.org/ip')
+    .then(function (response) {
+        console.log(response);
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
+
 promise.then(function (value){
     return value;
 }).catch(function (err) {
-    console.log(err);
+    console.log(err.code, err.message);
 });
 
 console.log('It is executed right after setTimeOut()');
