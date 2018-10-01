5161e423ac3303c976d04490002fd5216c76885e
robie01 <robie.sun.rs@gmail.com>
2018-10-01 11:58:54 +0200

    promise basics

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -6,7 +6,9 @@ if('serviceWorker' in navigator) {
         .register('/sw.js')
         .then(function () {
             console.log('service worker registered!');
-        });
+        }).catch(function (err) {
+            console.log(err);
+    });
 }
 window.addEventListener('beforeinstallprompt', function (event) {
     console.log('beforeinstallprompt fired');
@@ -16,3 +18,16 @@ window.addEventListener('beforeinstallprompt', function (event) {
     return false;
 });
 
+var promise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        resolve('This is executed once the timer is done')
+        reject({code: 500, message: 'An error occurred!'});
+    }, 3000);
+});
+promise.then(function (value){
+    return value;
+}).catch(function (err) {
+    console.log(err);
+});
+
+console.log('It is executed right after setTimeOut()');
